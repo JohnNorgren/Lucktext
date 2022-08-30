03ac88930fa971e5851d46df08a05de76848703e
John Norgren <john.norgren@elev.ga.ntig.se>
2022-08-30 10:26:43 +0200

    initial commit

---
+++ Lucktext/Program.cs
@@ -0,0 +1,16 @@
+﻿string name;
+string currentActivity;
+string 
+
+Console.WriteLine("Tell me your name before I throw you off this ship!");
+name = Console.ReadLine();
+
+Console.WriteLine("What are you doing here anyway?");
+currentActivity = Console.ReadLine();
+
+Console.WriteLine("");
+
+
+
+
+
